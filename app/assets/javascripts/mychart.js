1d241e79e76f5a25013c0001a22f0f48737a2cdf
vitalikost <vitalikostrovskij@yandex.ru>
2018-02-15 15:35:26 +0200

    add show 2 chart

--- app/assets/javascripts/mychart.js
+++ app/assets/javascripts/mychart.js
@@ -2,23 +2,50 @@ document.addEventListener("turbolinks:load", function() {
     // ...
 
 var labels = $('#chart-data').data('labels');
-var values = $('#chart-data').data('values');
-var label = $('#chart-data').data('label');
+var values1 = $('#chart-data').data('values1');
+var label1 = $('#chart-data').data('label1');
+var values2 = $('#chart-data').data('values2');
+var label2 = $('#chart-data').data('label2');
 if (labels != null) {
 
 
     setInterval(function() {location.reload()}, 1000*60*5);
 
-    new Chart(document.getElementById("line-chart"), {
+    new Chart(document.getElementById("line-chart1"), {
         type: 'line',
         data: {
             // labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
             labels: labels,
             datasets: [{
                 // data: [1,2,3],
-                data: values,
+                data: values1,
                 // label: "Africa",
-                label: label,
+                label: label1,
+
+                borderColor: "#3e95cd",
+                fill: false
+            }
+
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Показники:'
+            }
+        }
+    });
+
+    new Chart(document.getElementById("line-chart2"), {
+        type: 'line',
+        data: {
+            // labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+            labels: labels,
+            datasets: [{
+                // data: [1,2,3],
+                data: values2,
+                // label: "Africa",
+                label: label2,
 
                 borderColor: "#3e95cd",
                 fill: false
